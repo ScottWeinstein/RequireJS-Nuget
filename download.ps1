603f2b133aa4e22d730ecaaa91662bd03102ee92
Scott Weinstein <scottwww@squidnet.com>
2012-08-01 09:08:47 -0400

    update to 2.0.4

---
+++ download.ps1
@@ -0,0 +1,21 @@
+
+function download($url, $name)
+{
+    if (!$name)
+    {
+        $name = $url.split('/')[-1]        
+    }
+
+    $wc = New-Object net.webclient
+
+    $wc.DownloadFile($url, "$pwd\content\Scripts\$name")
+}
+$ver = "2.0.4"
+
+download "http://requirejs.org/docs/release/$ver/minified/require.js" require.min.js
+download "http://requirejs.org/docs/release/$ver/comments/require.js" require.js
+download "http://requirejs.org/docs/release/$ver/r.js"
+download "https://raw.github.com/requirejs/text/latest/text.js"
+download "https://raw.github.com/requirejs/domReady/latest/domReady.js"
+download "https://github.com/jrburke/require-cs/raw/latest/cs.js"
+download "https://raw.github.com/requirejs/i18n/latest/i18n.js"
