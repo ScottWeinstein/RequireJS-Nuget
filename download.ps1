1ccb062800e886e8bb5d548345994de3a14f846d
Scott Weinstein <scottwww@squidnet.com>
2012-08-04 09:47:47 -0400

    remove content, added by mistake. Download license. Add .gitignore

--- download.ps1
+++ download.ps1
@@ -12,6 +12,7 @@ function download($url, $name)
 }
 $ver = "2.0.4"
 
+mkdir "content\Scripts"
 download "http://requirejs.org/docs/release/$ver/minified/require.js" require.min.js
 download "http://requirejs.org/docs/release/$ver/comments/require.js" require.js
 download "http://requirejs.org/docs/release/$ver/r.js"
@@ -19,3 +20,5 @@ download "https://raw.github.com/requirejs/text/latest/text.js"
 download "https://raw.github.com/requirejs/domReady/latest/domReady.js"
 download "https://github.com/jrburke/require-cs/raw/latest/cs.js"
 download "https://raw.github.com/requirejs/i18n/latest/i18n.js"
+download "https://raw.github.com/jrburke/requirejs/master/LICENSE"
+move-item "$pwd\content\Scripts\LICENSE" "$pwd\content\Require.JS - LICENSE.txt" 
